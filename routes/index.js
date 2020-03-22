885b872d11cc1751b47e586e9c8fd1d14b7cbc3c
Vincent Schlosser <s.vincent@live.fr>
2020-03-22 15:11:04 +0100

    CRUD rooms

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
-const usersRouter = require('./users')
+const usersRouter = require('./users');
+const roomsRouter = require('./rooms');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ title: 'Welcome' });
 });
 
-usersRouter(router)
+usersRouter(router);
+
+roomsRouter(router);
 
 module.exports = router;
