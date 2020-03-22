257db4dbbabea9c54c41acf6662c0fac375b983b
Vincent Schlosser <s.vincent@live.fr>
2020-03-22 14:47:08 +0100

    CRUD user

--- routes/users.js
+++ routes/users.js
@@ -1,65 +1,107 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
+const fieldsReturn = 'firstName lastName email role';
+
 const users = (router) => {
-// Get all Users
+  // Get all Users
   router.get('/users', function (req, res, next) {
 
-    User.find(function (err, games) {
+    User.find(null, fieldsReturn, function (err, users) {
       if (err)
-        res.send(err);
+        console.log(err);
 
-      res.json(games);
+      res.json(users);
     });
 
   });
 
-// Get one User
+  // Get one User (By ID)
   router.get('/users/:id', function (req, res, next) {
 
     const id = req.params.id;
 
-    User.findOne({ id: id }, function (err, user) {
+    User.findById(id, fieldsReturn, function (err, user) {
       if (err)
-        res.send(err);
+        console.log(err);
 
       res.json(user);
     });
 
   });
 
-// Create User
-  router.post('/users', function (req, res, next) {
+  // Update User
+  router.put('/users/:id', function (req, res, next) {
 
     const saltRounds = 10;
-    const user = new User();
-    
-    user.firstName = req.body.firstName;
-    user.lastName = req.body.lastName;
-    user.password = req.body.password;
-    user.email = req.body.email;
-    user.role = req.body.role || "user";
+    const id = req.params.id;
 
-    bcrypt.genSalt(saltRounds, function(err, salt) {
+    User.findById(id, fieldsReturn, function (err, user) {
       if (err)
         console.log(err);
 
-      bcrypt.hash(user.password, salt, function (err, hash) {
-        if (err)
-          console.log(err);
-
-        user.password = hash;
-
-        user.save(function (err) {
+      if (user) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
           if (err)
-            res.send(err);
+            console.log(err);
 
-          res.json("User created");
+          bcrypt.hash(req.body.password, salt, function (err, hash) {
+            if (err)
+              console.log(err);
+
+            user.firstName = req.body.firstName;
+            user.lastName = req.body.lastName;
+            user.password = hash;
+            if (req.body.role)
+              user.role = req.body.role;
+
+            user.save(function (err, userUpdated) {
+              if (err)
+                console.log(err);
+
+              res.json("User updated");
+            });
+          });
         });
-      });
+      }
+      else {
+        res.json("No user found");
+      }
     });
 
   });
+
+  // Create User
+  router.post('/users', function (req, res, next) {
+
+    const user = new User();
+
+    user.email = req.body.email;
+    user.role = req.body.role || "user";
+
+
+    user.save(function (err, userUp) {
+      if (err)
+        console.log(err);
+
+      console.log(userUp)
+      res.json("User created");
+    });
+  });
+
+  // Delete User
+  router.delete('/users/:id', function (req, res, next) {
+
+    const id = { _id: req.params.id };
+
+    User.deleteOne(id, function (err, prom) {
+      if (err)
+        console.log(err);
+
+      res.json(prom);
+    });
+  });
+
 };
 
 module.exports = users;
