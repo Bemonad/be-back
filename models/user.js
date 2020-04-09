608b4e2d9f7872e5d43a70ff8ede0be389866465
Vincent Schlosser <s.vincent@live.fr>
2020-04-09 11:02:05 +0200

    Add intelligence in user's model

--- models/user.js
+++ models/user.js
@@ -1,15 +1,74 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
     password: String,
-    email: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     role: String,
     token: {
-        type: String,
-        required: true
+        type: String
     }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema );
+userSchema.pre('save', async function (next) {
+
+    const user = this;
+
+    if (user.isModified('password')) {
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        user.password = await bcrypt.hash(user.password, salt);
+    }
+
+    next()
+});
+
+userSchema.methods.generateAuthToken = async function() {
+
+    const user = this;
+    const token = jwt.sign({_id: user._id, email: user.email}, process.env.JWT_KEY);
+
+    user.token = token;
+
+    await user.save();
+
+    return token
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+
+    const user = await User.findOne({ email} );
+    if (!user) {
+        throw new Error({ error: 'Invalid login credentials' })
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+        throw new Error({ error: 'Invalid login credentials' })
+    }
+    return user
+};
+
+userSchema.statics.findByJWT = (token) => {
+    const data = jwt.verify(token, process.env.JWT_KEY);
+
+    return User.findOne({ _id: data._id, token: token });
+};
+
+const User = mongoose.model('User', userSchema);
+
+
+module.exports = User;
