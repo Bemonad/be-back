fd9a1b45bda3f7183064a38522474bdc0831d953
Vincent Schlosser <s.vincent@live.fr>
2020-04-18 12:50:22 +0200

    Create booking middleware

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,7 +1,28 @@
 const Booking = require('../models/booking');
 const crud = require('../services/crud');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const bookings = (router) => {
+
+  router.use('/bookings', async (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+
+    if (token) {
+      try {
+        jwt.verify(token, process.env.JWT_KEY);
+        const user = await User.findByJWT(token);
+        if (user) {
+          next();
+        } else {
+          res.status(401).json({msg: 'You need to log in.'});
+        }
+      } catch (e) {
+        res.status(401).json({msg: 'You need to log in.'});
+      }
+    }
+  });
+
   router.use('/bookings', crud(Booking));
 };
 
