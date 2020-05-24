8519aec8809bdfbb21d17945aac89022fc1c6ead
Vincent Schlosser <s.vincent@live.fr>
2020-05-24 18:32:22 +0200

    Add role in jwt

--- models/user.js
+++ models/user.js
@@ -39,7 +39,7 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.generateAuthToken = async function() {
 
     const user = this;
-    const token = jwt.sign({_id: user._id, email: user.email}, process.env.JWT_KEY, { expiresIn: '4h' });
+    const token = jwt.sign({_id: user._id, email: user.email, role: user.role}, process.env.JWT_KEY, { expiresIn: '4h' });
 
     user.token = token;
 
